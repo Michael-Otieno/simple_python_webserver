34946df11ddeac5e447d72df0460a14c947549cf
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-01-15 12:58:52 +0300

    add path to new item

--- dynamicHtmlWebServer.py
+++ dynamicHtmlWebServer.py
@@ -2,7 +2,7 @@ import cgi
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 #Shopping list
-shoppingList = ['Wheat','Flour','Cooking oil']
+shoppinglist = ['Wheat','Flour','Cooking oil']
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -15,13 +15,31 @@ class Server(BaseHTTPRequestHandler):
             output += '<html><body>'
             output += '<h1>Shopping List</h1>'
             output += '<h3><a href="/shoppinglist/new">Add New Item</a></h3>'
-            for shopping in shoppingList:
+            for shopping in shoppinglist:
                 output +=shopping
-                # output += '<a href="/tasklist/%s/remove"> X </a>' % shopping
+                output += '<a href="/shoppinglist/%s/remove"> X </a>' % shopping
                 output += '</br>'
             output += '</body></html>'
             self.wfile.write(output.encode())
 
+        if self.path.endswith('/new'):
+            self.send_response(200)
+            self.send_header('content-type','text/html')
+            self.end_headers()
+
+            output = ''
+            output += '<html><body>'
+            output += '<h1>Add New Item</h1>'
+            
+            output += '<form method="POST" enctype="multipart/form-data" action="/shoppinglist/new">'
+            output += '<input name="item" type="test" placeholder="Add new item">'
+            output += '<input type="submit" value="Add Item">'
+            output += '</form>'
+            output += '</body></html>'
+
+            self.wfile.write(output.encode())
+
+        
 
 def main():
     PORT = 9000
