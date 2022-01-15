fa6234456ef906d69232cbeb9e185c4cd61930e3
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-01-15 12:40:14 +0300

    add server class with do_get request

--- dynamicHtmlWebServer.py
+++ dynamicHtmlWebServer.py
@@ -1,1 +1,34 @@
-#Shopping list tracker
+import cgi
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+#Shopping list
+shoppingList = ['Wheat','Flour','Cooking oil']
+
+class Server(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path.endswith('/shoppinglist'):
+            self.send_response(200)
+            self.send_header('content-type','text/html')
+            self.end_headers()
+
+            output = ''
+            output += '<html><body>'
+            output += '<h1>Shopping List</h1>'
+            output += '<h3><a href="/shoppinglist/new">Add New Item</a></h3>'
+            for shopping in shoppingList:
+                output +=shopping
+                # output += '<a href="/tasklist/%s/remove"> X </a>' % shopping
+                output += '</br>'
+            output += '</body></html>'
+            self.wfile.write(output.encode())
+
+
+def main():
+    PORT = 9000
+    server_address = ('localhost', PORT)
+    server = HTTPServer(server_address, Server)
+    print('Server running on port %s' % PORT)
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
